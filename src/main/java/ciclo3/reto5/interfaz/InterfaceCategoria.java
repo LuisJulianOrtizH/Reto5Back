648235c758d651d6df311eb8b6d429a891f958b1
JulianOrtiz <Luisjulianortizh@gmail.com>
2021-11-01 21:58:31 -0500

    comentario

---
+++ src/main/java/ciclo3/reto5/interfaz/InterfaceCategoria.java
@@ -0,0 +1,13 @@
+package ciclo3.reto5.interfaz;
+
+import ciclo3.reto5.modelo.Categoria;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author JULIANORTIZH
+ */
+
+public interface InterfaceCategoria extends CrudRepository<Categoria,Integer> {
+
+}
---
+++ src/main/java/ciclo3/reto5/interfaz/InterfaceCliente.java
@@ -0,0 +1,13 @@
+package ciclo3.reto5.interfaz;
+
+import ciclo3.reto5.modelo.Cliente;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author JULIANORTIZH
+ */
+
+public interface InterfaceCliente extends CrudRepository<Cliente,Integer>{
+
+}
